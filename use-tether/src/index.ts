8ac403d185b4dfa2b97316e0ccc233073731904b
Rainnny7 <braydonrainnny@gmail.com>
2024-09-10 15:47:22 -0400

    Fix TS index

--- use-tether/src/index.ts
+++ use-tether/src/index.ts
@@ -4,4 +4,18 @@ export * from "@/hook/websocket";
 // Types
 export * from "@/types/config";
 export * from "@/types/snowflake";
-export * from "@/types/user/user";
+
+export * from "@/types/user/discord-user";
+export * from "@/types/user/user-flags";
+export * from "@/types/user/avatar/avatar";
+export * from "@/types/user/avatar/decoration-asset";
+export * from "@/types/user/avatar/avatar-decoration";
+export * from "@/types/user/banner";
+export * from "@/types/user/custom-status";
+export * from "@/types/user/spotify-activity";
+export * from "@/types/user/badge/user-badge-icon";
+export * from "@/types/user/badge/user-badge";
+export * from "@/types/user/connected-account";
+export * from "@/types/user/clan/clan-badge";
+export * from "@/types/user/clan/clan";
+export * from "@/types/user/nitro-subscription";
