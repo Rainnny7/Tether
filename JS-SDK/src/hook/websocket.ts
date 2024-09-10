1f0b99012488b581c47082d6679353eed6925d89
Rainnny7 <braydonrainnny@gmail.com>
2024-09-09 20:34:01 -0400

    this should work? (pls)

--- JS-SDK/src/hook/websocket.ts
+++ JS-SDK/src/hook/websocket.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Snowflake } from "@/types/snowflake";
 import { TetherConfig } from "@/types/config";
+import { DiscordUser } from "@/types/user";
 
 export const useTetherWS = (
     snowflake: Snowflake,
@@ -8,12 +9,42 @@ export const useTetherWS = (
         endpoint: "usetether.rest",
         secure: true,
     }
-): Snowflake => {
-    const [user] = useState<Snowflake>(snowflake);
+): DiscordUser | undefined => {
+    const [user] = useState<DiscordUser | undefined>();
 
+    const url: string = `ws${secure && "s"}://${endpoint}/gateway`;
     useEffect(() => {
-        console.log("HELLO WORLD", endpoint, secure);
-    }, [snowflake]);
+        // Prevent from running on the server
+        if (typeof window === "undefined") {
+            return;
+        }
+        let socket: WebSocket; // The current WebSocket connection
+
+        /**
+         * Establish a connection with the API.
+         */
+        function connect() {
+            console.log("[Tether] Connecting to the WebSocket server...");
+            socket = new WebSocket(url); // Connect to the gateway
+            socket.addEventListener("open", () => {
+                console.log("[Tether] WebSocket connection established!");
+                console.log("attempt to lsn to", snowflake);
+            });
+            // socket.addEventListener("close", connect);
+
+            socket.addEventListener("message", (event) => {
+                console.log("data:", event.data);
+            });
+        }
+
+        connect();
+
+        // Cleanup
+        return () => {
+            socket.removeEventListener("close", connect);
+            socket.close();
+        };
+    }, [url]);
 
     return user;
 };
