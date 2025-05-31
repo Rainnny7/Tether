eedd21869631841f1b4eca1187936a2143a9bbcc
bray <braydonrainnny@gmail.com>
2025-05-31 05:28:48 -0400

    prod debug

--- API/src/main/java/me/braydon/tether/service/DiscordService.java
+++ API/src/main/java/me/braydon/tether/service/DiscordService.java
@@ -201,6 +201,7 @@ public final class DiscordService extends ListenerAdapter {
      */
     @NonNull
     private JSONObject getUser(long snowflake, boolean includeProfile) throws BadRequestException {
+        System.out.println("userAccountToken = " + userAccountToken);
         HttpResponse<JsonNode> response = Unirest.get("https://discord.com/api/v10/users/" + snowflake + (includeProfile ? "/profile?with_mutual_guilds=false" : ""))
                 .header(HttpHeaders.AUTHORIZATION, userAccountToken).asJson();
         JSONObject json = response.getBody().getObject();
